a0164623078ce22bf2079c0f94486c7042b904e8
Abai Abakirov <abaikirov@gmail.com>
2018-12-20 22:01:07 +0600

    Initial Commit

---
+++ NeuralNetwork/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NeuralNetwork
+//
+//  Created by Abai Abakirov on 12/20/18.
+//  Copyright © 2018 abaikirov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
